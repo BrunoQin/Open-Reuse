7a40972ce68dc523664f3ad3259352ad346b5c99
Cao Qi <caoqiyeah2014@gmail.com>
2016-05-14 17:19:08 +0800

    improve test

--- open-reuse-server/src/test/java/com/openreuse/server/test/basic/LoginRouteTest.java
+++ open-reuse-server/src/test/java/com/openreuse/server/test/basic/LoginRouteTest.java
@@ -8,8 +8,6 @@ import com.openreuse.server.group.GroupManager;
 import com.openreuse.server.request.route.LoginRoute;
 import org.junit.Test;
 
-import java.util.List;
-
 /**
  * Created by Jasmine on 16/5/14.
  */
@@ -18,15 +16,13 @@ public class LoginRouteTest {
     public void testRoute(){
         Message loginMessage = MessageBuilder.messageBuilder().setFrom("Jasmine").setBody("Jasmine"+"\n"+"1234").setType(MessageType.LOGIN_MESSAGE).build();
         LoginRoute route = new LoginRoute();
+        GroupManager groupManager = GroupManager.getInstance();
+        Group groupToAdd = new Group(1);
+        groupToAdd.userList.add("Bruno");
+        groupManager.addUserGroupMapping("Jasmine",groupToAdd);
+
         boolean result = route.route(loginMessage);
 
-        if(result==true){
-            List<Group> groupList = GroupManager.getInstance().getUserGroupMapping("Jasmine");
-            for(Group group : groupList){
-                System.out.print(group.userList.toString());
-            }
-        }
-       else
-            System.out.print(result);
+        System.out.print(result);
     }
 }
